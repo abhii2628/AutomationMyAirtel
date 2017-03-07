43eb2c84a26976cb077631dd6ccebec4c39596c2
abhii2628 <B0095770@Abhisheks-MAC-2.local>
2017-03-07 23:21:49 +0530

    Util class, Load Config, Changes to manager

--- src/main/java/core/managers/TestManager.java
+++ src/main/java/core/managers/TestManager.java
@@ -6,16 +6,20 @@ import com.relevantcodes.extentreports.LogStatus;
 import core.MyLogger;
 import core.Retry;
 import core.TestInfo;
+import io.appium.java_client.android.AndroidDriver;
+import org.apache.commons.io.FileUtils;
+import org.junit.AssumptionViolatedException;
 import org.junit.Rule;
 import org.junit.rules.TestRule;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import ui.android.AndroidInit;
+import ui.apps.myairtelapp.utils.UtilDriver;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.io.File;
+import java.io.IOException;
 
 
 /**
@@ -26,7 +30,7 @@ public class TestManager {
 
     public static TestInfo testInfo = new TestInfo();
     private static String bn = "152";
-    private String filenameOfReport = "/Users/abhishek/Desktop/reports/FirstReport" +".html";
+    private String filenameOfReport = "/Users/abhishek/Desktop/reports/testRun" + ".html";
 
     @Rule
     public Retry retry = new Retry(1);
@@ -37,14 +41,14 @@ public class TestManager {
         @Override
         public void failed(Throwable t, Description description) {
             MyLogger.log.info("Test Failed:");
-            AndroidInit.adb.takeScreenshot("/Users/abhishek/Desktop/screenshots"+System.currentTimeMillis()+".png");
+            AndroidInit.adb.takeScreenshot("/Users/abhishek/Desktop/screenshots" + System.currentTimeMillis() + ".png");
             TestInfo.printResults();
             ExtentReports extent = createReport();
-            ExtentTest test = extent.startTest(description.getDisplayName(), "Test failed, click here for further details");
+            ExtentTest test = extent.startTest(description.getDisplayName(), testInfo.name());
             // step log
-            test.log(LogStatus.FAIL, "Failure trace Appium: " + t.toString());
-            test.log(LogStatus.FAIL,t.toString(),description.getClassName());
-            test.addScreenCapture("/Users/abhishek/Desktop/screenshots");
+            test.log(LogStatus.FAIL, t.toString(), description.getClassName());
+            //test.log(LogStatus.FAIL, t.getStackTrace().toString(),description.getClassName());
+            test.addScreenCapture(UtilDriver.CaptureScreen(AndroidInit.driver,"/Users/abhishek/Desktop/screenshots/") +System.currentTimeMillis());
             flushReports(extent, test);
         }
 
@@ -52,11 +56,25 @@ public class TestManager {
         public void succeeded(Description description) {
             MyLogger.log.info("Test Passed:");
             TestInfo.printResults();
-            AndroidInit.adb.takeScreenshot("/Users/abhishek/Desktop/screenshots"+System.currentTimeMillis()+".png");
+            AndroidInit.adb.takeScreenshot("/Users/abhishek/Desktop/screenshots" + System.currentTimeMillis() + ".png");
             ExtentReports extent = createReport();
-            ExtentTest test = extent.startTest(description.getDisplayName(),testInfo.name());
+            ExtentTest test = extent.startTest(description.getDisplayName(), testInfo.name());
             // step log
             test.log(LogStatus.PASS, "Test passed");
+            test.addScreenCapture(UtilDriver.CaptureScreen(AndroidInit.driver,"/Users/abhishek/Desktop/screenshots/") +System.currentTimeMillis());
+            flushReports(extent, test);
+        }
+
+        @Override
+        protected void skipped(AssumptionViolatedException e, Description description) {
+            MyLogger.log.info("Test Failed:");
+            AndroidInit.adb.takeScreenshot("/Users/abhishek/Desktop/screenshots" + System.currentTimeMillis() + ".png");
+            TestInfo.printResults();
+            ExtentReports extent = createReport();
+            ExtentTest test = extent.startTest(description.getDisplayName(), "Test skipped due to the logic written");
+            // step log
+            test.log(LogStatus.SKIP, "Failure trace Appium: " + e.toString());
+            test.log(LogStatus.SKIP, e.toString(), description.getClassName());
             test.addScreenCapture("/Users/abhishek/Desktop/screenshots");
             flushReports(extent, test);
         }
@@ -70,6 +88,7 @@ public class TestManager {
                     .addSystemInfo("OS version", AndroidInit.adb.getAndroidVersionAsString())
                     .addSystemInfo("Device Manufacturer", AndroidInit.adb.getDeviceManufacturer())
                     .addSystemInfo("Build number", bn);
+            extent.loadConfig(new File(System.getProperty("user.dir") + "/src/resources/LoadConfig.xml"));
             return extent;
         }
 
@@ -81,6 +100,7 @@ public class TestManager {
             extent.close();
         }
 
+
     /*    private String getDateTime() {
             Date date = Calendar.getInstance().getTime();
             // Display a date in day, month, year format
@@ -89,7 +109,11 @@ public class TestManager {
             MyLogger.log.info("Date is " + today);
             return today;
         }
+
 */
     };
+
+
+
 }
 
